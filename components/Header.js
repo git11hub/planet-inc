0fb8b5d6d404a28bdf70370f3fe47a5be6702b56
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-08-12 01:09:37 +0600

    finish without download

--- components/Header.js
+++ components/Header.js
@@ -1,101 +1,115 @@
-import React, { useState } from 'react';
-import earthImg from '../images/earth.jpeg';
-import jupiterImg from '../images/jupiter.jpeg';
-import marsImg from '../images/mars.jpeg';
-import mercuryImg from '../images/mercury.jpeg';
-import saturnImg from '../images/saturn.jpeg';
-import venusImg from '../images/venus.jpeg';
-import Planet from './Planet';
-import noImg from '../images/noimg.jpg';
-
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import earthImg from "../images/earth.jpeg";
+import jupiterImg from "../images/jupiter.jpeg";
+import marsImg from "../images/mars.jpeg";
+import mercuryImg from "../images/mercury.jpeg";
+import saturnImg from "../images/saturn.jpeg";
+import venusImg from "../images/venus.jpeg";
+import Planet from "./Planet";
+import noImg from "../images/noimg.jpg";
 
 const planetImage = {
-    earth: earthImg,
-    jupiter: jupiterImg,
-    mars: marsImg,
-    mercury: mercuryImg,
-    saturn: saturnImg,
-    venus: venusImg,
-    noImg: noImg
-}
+  earth: earthImg,
+  jupiter: jupiterImg,
+  mars: marsImg,
+  mercury: mercuryImg,
+  saturn: saturnImg,
+  venus: venusImg,
+};
 
 function Header() {
+  const [planet, setPlanet] = useState([]);
+  const [input, setInput] = useState("");
 
-    const [planet, setPlanet] = useState([]);
-    const [input, setInput] = useState("");
+  const storePlanet = (event) => {
+    event.preventDefault();
 
-    console.log(planet);
+    var nameArr = input.split(",");
+    setPlanet(nameArr);
 
-    const aOk = (event) =>{
-        event.preventDefault();
-        // setPlanet(input)
+    setInput("");
+  };
 
-        var nameArr = input.split(',');
-        setPlanet(nameArr);
+  // Planet Image
+  const planetImg = (singlePlanet) => {
+    const lowerCasePlanet = singlePlanet.toLowerCase();
+    return planetImage[lowerCasePlanet.replace(/\s/g, "")] || noImg;
+  };
 
-        setInput("");
-    }
+  // Drag and Drop...
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const items = Array.from(planet);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
 
-    // again againTry
-    const againTry = (singlePlanet) => {
-        const lowerCasePlanet = singlePlanet.toLowerCase();
-        return planetImage [lowerCasePlanet.replace(/\s/g, '')]
-    }
+    setPlanet(items);
+  }
 
-    function handleOnDragEnd (result) {
-        // console.log(result);
-        if (!result.destination) return;
-        const items = Array.from(planet);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
+  return (
+    <header className="container mx-auto w-1/3" download>
+      <form className="mt-5">
+        <label>
+          Planet:
+          <input
+            className="border border-gray-300 py-1 px-4 ml-1 rounded"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            type="text"
+            name="name"
+            placeholder=" Type a planet..."
+          />
+        </label>
+        <input
+          className="border border-indigo-600 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 ml-1 rounded"
+          onClick={storePlanet}
+          type="submit"
+          value="Generate"
+        />
+      </form>
 
-        setPlanet(items);
-    }
-    
-    return (
-        <header className="container mx-auto w-1/3" download>
-            <form>
-                <label>
-                    Planet:
-                    <input 
-                    value={input}
-                    onChange={(event) => setInput(event.target.value)}
-                    type="text" 
-                    name="name" 
-                    placeholder=" Type a planet..."/>
-                </label>
-                <input onClick={aOk} type="submit" value="Generate" />
-            </form>
-            {/* {planet.map(singlePlanet => <h1 key={singlePlanet}>{singlePlanet}</h1>)} */}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {planet.map((singlePlanet, index) => (
+                <Draggable
+                  key={singlePlanet}
+                  draggableId={singlePlanet}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <Planet
+                        singlePlanet={singlePlanet}
+                        singleImg={planetImg(singlePlanet)}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
 
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="characters">
-                    {(provided)=>(
-                <div {...provided.droppableProps} ref={provided.innerRef}>
-                    {planet.map((singlePlanet, index) => <Draggable key={singlePlanet} draggableId={singlePlanet} index={index}>
-                        {(provided)=>(
-                            <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                            <Planet singlePlanet={singlePlanet} singleImg={againTry(singlePlanet)}/>
-                            </div>
-                        )}
-                        </Draggable>
-                        )
-                        }
-                      {provided.placeholder}  
-                </div>
-                )}
-                
-                </Droppable>
-            </DragDropContext>
-
-            {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="path/to/file.ext" download>Download this page</button> */}
-
-            <button className="mt-5" type="submit">
-                <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="#" download="page.html">Download</a>
-            </button>
-        </header>
-    )
+      <button className="my-5" type="submit">
+        <a
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          href="#"
+          download="page.html"
+        >
+          Download
+        </a>
+      </button>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- components/Planet.js
+++ components/Planet.js
@@ -1,22 +1,22 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-// const matchedPlanet = () => {
-//     planetImage.match(/earth/g)
-// }
-
-function Planet({singlePlanet, singleImg}) {
-
-    return (
-        <div className="grid grid-cols-2 h-32 border border-gray-200 my-4">
-            <div className="relative">
-                {singleImg && (
-                    <Image alt="planet" width={180} height={126} class="absolute top-0 left-0 w-full h-32 object-cover" src={singleImg} />
-                )}
-            </div>
-            <p className="text-3xl font-bold text-left p-10">{singlePlanet}</p>
-            {console.log(singleImg)}
-        </div>
-    )
+function Planet({ singlePlanet, singleImg }) {
+  return (
+    <div className="grid grid-cols-2 h-32 border border-gray-200 my-4">
+      <div className="relative">
+        {singleImg && (
+          <Image
+            alt="planet"
+            width={180}
+            height={126}
+            className="absolute top-0 left-0 w-full h-32 object-cover"
+            src={singleImg}
+          />
+        )}
+      </div>
+      <p className="text-3xl font-bold text-left p-10">{singlePlanet}</p>
+    </div>
+  );
 }
 
-export default Planet
+export default Planet;
